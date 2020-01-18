f86c15edb069af6120b2bb6c7bb4197932de0d80
giuliom95 <giuliomartella1995@gmail.com>
2020-01-18 13:01:53 +0100

    Basic music reproduction

---
+++ src/audiohandler.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+import simpleaudio as sa
+
+import subprocess
+import pathlib
+import os
+import threading
+
+import musicdb
+
+class AudioHandler():
+
+    def __init__(self):
+        self._waveobj = None
+        self._playobj = None
+        self._thread = None
+        self._bufferfilepath = str(pathlib.Path('./buffer.wav'))
+
+    def playtrack(self, path: pathlib.Path):
+        subprocess.Popen([
+            'ffmpeg',
+            '-i', str(path),
+            '-f', 'wav',
+            '-c:a', 'pcm_s16le', 
+            '-y', self._bufferfilepath
+        ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL)
+
+        self._thread = threading.Thread(target=self._playtrackaftermath)
+        self._thread.start()
+
+    def _playtrackaftermath(self):
+        while True:
+            try:
+                # Check if ffmpeg has written something to disk yet
+                bufferinfo = os.stat(self._bufferfilepath)
+                if bufferinfo.st_size > 0:
+                    self._waveobj = sa.WaveObject.from_wave_file(self._bufferfilepath)
+                    self._playobj = self._waveobj.play()
+                    break
+            except Exception as e:
+                if not isinstance(e, FileNotFoundError):
+                    raise
+
+
+if __name__=='__main__':
+    ah = AudioHandler()
+    ah.playtrack(pathlib.Path('/home/gmartell/Music/bak/05 - Poor Leno.mp3'))
+
+    while True:
+        pass
